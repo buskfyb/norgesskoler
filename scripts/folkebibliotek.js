0e23abe63eddaa7d09684fd12d428d11efc60e35
buskfyb <buskfyb@bfk.no>
2019-11-13 16:06:36 +0100

    Update folkebibliotek.js

--- scripts/folkebibliotek.js
+++ scripts/folkebibliotek.js
@@ -38,7 +38,10 @@ bibliotekCircle = new L.circleMarker([folkebibliotekArray[i][0], folkebibliotekA
 	}).bindPopup(folkebibliotekArray[i][4]); //add the text from the array for each library to a popup
 
 	//add it to the folkebibliotek overlay group
-	bibliotekCircle.addTo(folkebibliotekGroup);
+	var bibliotekSquare = L.rectangle(bibliotekCircle.getBounds());
+	bibliotekSquare.addTo(folkebibliotekGroup);
+	//bibliotekCircle.addTo(folkebibliotekGroup);
+
 	
 } //end for loop
 
